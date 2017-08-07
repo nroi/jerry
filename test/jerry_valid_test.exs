5d25e63f08adee23cc7ea3a36ccf0fd963dbfb75
Fabian Muscariello <nroi@mailbox.org>
2017-08-07 23:46:44 +0200

    Add test case

--- test/jerry_valid_test.exs
+++ test/jerry_valid_test.exs
@@ -437,4 +437,33 @@ defmodule JerryValidTest do
     assert toml == expected
   end
 
+  test "toml-example-github" do
+    toml = File.read!("test/valid/toml-example-github.toml") |> Jerry.decode!
+    expected = %{
+      "clients" => %{
+        "data" => [["gamma", "delta"], [1, 2]],
+        "hosts" => ["alpha", "omega"]
+      },
+      "database" => %{
+        "connection_max" => 5000,
+        "enabled" => true,
+        "ports" => [8001, 8001, 8002],
+        "server" => "192.168.1.1"},
+      "owner" => %{
+        "dob" => {:toml_datetime, "1979-05-27T07:32:00-08:00"},
+        "name" => "Tom Preston-Werner"
+      },
+      "servers" => %{
+        "alpha" => %{
+          "dc" => "eqdc10", "ip" => "10.0.0.1"
+        },
+        "beta" => %{
+          "dc" => "eqdc10",
+          "ip" => "10.0.0.2"}
+      },
+      "title" => "TOML Example"
+    }
+    assert toml == expected
+  end
+
 end

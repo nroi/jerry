fb13b13408e288e5b7955fa9abf732a7c7cbc184
Fabian Muscariello <nroi@mailbox.org>
2017-09-16 21:04:45 +0200

    Update mix.exs before publishing

--- mix.exs
+++ mix.exs
@@ -8,31 +8,36 @@ defmodule Jerry.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
+     description: description(),
+     package: package(),
+     deps: deps(),
+     name: "Jerry",
+     source_url: "https://github.com/nroi/jerry",
      test_coverage: [tool: ExCoveralls],
      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:excoveralls, "~> 0.7", only: :test}
+    ]
+  end
+
+  defp description() do
+    "A TOML parser"
+  end
+
+  defp package() do
+    [
+      maintainers: ["Fabian Muscariello"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nroi/jerry"}
     ]
   end
 end
